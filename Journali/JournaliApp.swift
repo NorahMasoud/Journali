6db0e8ffdf3c62961300f7b331350920ad039498
Norah <norah.masoud@hotmail.com>
2025-10-18 15:30:14 +0300

    Initial Commit

---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by Norah Masoud Aloqayli on 26/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
